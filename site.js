bbbb684c38899fcc2bb9921bbd2ad7a29176a49a
Mikaisdying <phphuong19102002@gmail.com~>
2023-11-08 23:51:24 +0700

    Chỉnh thêm ảnh, thêm nút đầu hàng, win/lost notice

--- site.js
+++ site.js
@@ -4,7 +4,25 @@ function setActiveButton(index) {
         buttons[i].classList.remove('active');
     }
     buttons[index].classList.add('active');
+
+    var difficultyOverlay = document.getElementById('difficultyOverlay');
+
+    switch (index) {
+        case 0:
+            difficultyOverlay.textContent = '-Easy-';
+            break;
+        case 1:
+            difficultyOverlay.textContent = 'Medium';
+            break;
+        case 2:
+            difficultyOverlay.textContent = '-Hard-';
+            break;
+        default:
+            break;
+    }
 }
+
+
 var audio = document.getElementById("myAudio");
 var soundIcon = document.getElementById("soundIcon");
 var soundIconIcon = document.getElementById("soundIconIcon");
@@ -23,13 +41,15 @@ function toggleSound() {
   }
 }
 
-document.getElementById("homeButton").addEventListener("click", function() {
-  location.reload(); 
+var homeButtons = document.querySelectorAll('.homeButton');
+
+homeButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        location.reload();
+    });
 });
 
-document.getElementById("logo").addEventListener("click", function() {
-  location.reload(); 
-});
+
 
 document.getElementById('size').addEventListener('change', function() {
   var sizeInput = this.value;
@@ -44,4 +64,35 @@ document.getElementById('size').addEventListener('change', function() {
 
 document.getElementById('close-button').addEventListener('click', function() {
   document.getElementById('warning-message').style.display = 'none';
+});
+
+var dropContainer = document.getElementById("dropcontainer");
+var fileInput = document.getElementById("images");
+
+dropContainer.addEventListener("dragover", function (e) {
+  // prevent default to allow drop
+  e.preventDefault();
+}, false);
+
+dropContainer.addEventListener("dragenter", function () {
+  dropContainer.classList.add("drag-active");
+});
+
+dropContainer.addEventListener("dragleave", function () {
+  dropContainer.classList.remove("drag-active");
+});
+
+dropContainer.addEventListener("drop", function (e) {
+  e.preventDefault();
+  dropContainer.classList.remove("drag-active");
+  fileInput.files = e.dataTransfer.files;
+});
+
+var giveupButton = document.getElementById('giveupButton');
+var lostNotice = document.getElementById('lostNotice');
+
+giveupButton.addEventListener('click', function() {
+  lostNotice.style.display = 'block';
+  var imageOverlay = document.getElementById('imageOverlay');
+  imageOverlay.style.display = 'block';
 });
